fdda14c67173c4aca78c9653f9f5072fe1c9abdd
大星 <woqumaigejuzi@163.com>
2026-06-18 17:53:45 +0800

    添加网络相关

---
+++ scripts/init-server.sh
@@ -0,0 +1,9 @@
+# 1. 将 BBR 配置写入系统设置
+echo "net.core.default_qdisc=fq" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
+
+# 2. 使配置生效
+sudo sysctl -p
+
+# 3. 验证是否开启成功
+sysctl net.ipv4.tcp_congestion_control
