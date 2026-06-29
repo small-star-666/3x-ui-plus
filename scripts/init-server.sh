826edf3f91122bf4c96a4de57fe5d5872135f036
daxing <woqumaigejuzi@163.com>
2026-06-29 18:35:19 +0800

    适配域名

--- scripts/init-server.sh
+++ scripts/init-server.sh
@@ -1,9 +1,28 @@
+# 安装docker
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
+
+# 安装git
+sudo apt update
+sudo apt install git -y
+git clone https://github.com/small-star-666/3x-ui-plus.git
+
+
 # 1. 将 BBR 配置写入系统设置
 echo "net.core.default_qdisc=fq" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
-
 # 2. 使配置生效
 sudo sysctl -p
-
 # 3. 验证是否开启成功
 sudo sysctl net.ipv4.tcp_congestion_control
